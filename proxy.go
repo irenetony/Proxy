93912cfe71ae08d6b54eaa883d1f56f2a2b12859
Irene Tony <tonyi@tcd.ie>
2019-02-25 01:20:10 +0000

    Can block user entered urls

--- proxy.go
+++ proxy.go
@@ -1,1 +1,98 @@
+package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func handleTunneling(w http.ResponseWriter, r *http.Request) {
+	destConn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+	clientConn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+	go transfer(destConn, clientConn)
+	go transfer(clientConn, destConn)
+}
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
+func handleHTTP(w http.ResponseWriter, req *http.Request) {
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+func main() {
+	fmt.Println("Enter URLs to block seperated by commas:")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	URLs := strings.Split(input, ",")
+
+	//end := ":443"
+	//input1 := input + end
+	//fmt.Print(strings.TrimSpace(input1))
+	var proto string
+	flag.StringVar(&proto, "proto", "https", "Proxy protocol (http or https)")
+	flag.Parse()
+	if proto != "http" && proto != "https" {
+		log.Fatal("Protocol must be either http or https")
+	}
+	server := &http.Server{
+		Addr: ":4000",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("Requested URL: " + r.URL.String())
+			fmt.Println("Blocked: " + input)
+			//reqURI := r.URL.String()
+			blocked := false
+			for i := range URLs {
+				reqURI := r.URL.String()
+				if strings.TrimSpace(URLs[i]) == reqURI {
+					fmt.Println("This site is blocked")
+					blocked = true
+				}
+			}
+			if r.Method == http.MethodConnect && !blocked {
+				handleTunneling(w, r)
+				fmt.Println(r)
+			} else {
+				handleHTTP(w, r)
+			}
+
+		}),
+	}
+	log.Fatal(server.ListenAndServe())
+
+}
